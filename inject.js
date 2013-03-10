7c165befc236baadc5387dc152fe0034531dc874
Simon Oxtoby <soxtoby@zaptechnology>
2013-03-10 18:41:51 +1000

    Adding some basic container building and type registration.

--- inject.js
+++ inject.js
@@ -1,5 +1,70 @@
 ﻿(function(global) {
     'use strict';
-    
-    
+
+    var uid = 1;
+
+    function Builder() {
+        this._registrations = [];
+    };
+
+    Builder.prototype = {
+        build: function() {
+            return new Container(this._registrations);
+        },
+        
+        register: function (constructor) {
+            var registration = new Registration(constructor);
+            this._registrations.push(registration);
+            return registration;
+        }
+    };
+
+    function Registration(constructor) {
+        this.constructor = constructor;
+        this.registeredAs = [];
+    }
+
+    Registration.prototype = {
+        as: function (interfaces) {
+            this.registeredAs = this.registeredAs.concat(interfaces);
+        },
+        
+        asPrototypes: function() {
+            var prototype = this.constructor.prototype;
+            var constructor = prototype.constructor;
+            this.registeredAs.push(constructor);
+        }
+    };
+
+    function Container(registrations) {
+        this._registrations = {};
+        registrations.forEach(function (registration) {
+            registration.registeredAs.forEach(function(type) {
+                this._registrations[getOrCreateKey(type)] = registration;
+            }, this);
+        }, this);
+    };
+
+    Container.prototype = {
+        resolve: function (constructor) {
+            var key = getKey(constructor);
+            if (this._registrations[key])
+                return new this._registrations[key].constructor();
+            return new constructor();
+        }
+    };
+
+    function getOrCreateKey(type) {
+        if (!type.$injectId)
+            type.$injectId = uid++;
+        return type.$injectId;
+    }
+
+    function getKey(type) {
+        return type.$injectId;
+    }
+
+    global.Inject = {
+        Builder: Builder
+    };
 })(window);
--- tests.js
+++ tests.js
@@ -1,2 +1,58 @@
-﻿describe("inject", function () {
+﻿describe("injection", function() {
+    var builder = new Inject.Builder();
+
+    when("nothing registered", function() {
+        var sut = builder.build();
+
+        when("resolving existing class", function() {
+            function existingClass() { };
+            var result = sut.resolve(existingClass);
+
+            it("instantiates existing class", function() {
+                result.should.be.an.instanceOf(existingClass);
+            });
+        });
+    });
+
+    when("subtype is registered as super type", function() {
+        function superClass() { }
+        function subClass() { }
+        subClass.prototype = new superClass();
+
+        builder.register(subClass).as(superClass);
+        var sut = builder.build();
+
+        when("resolving super type", function() {
+            var result = sut.resolve(superClass);
+
+            it("instantiates the sub class", function() {
+                result.should.be.an.instanceOf(subClass);
+            });
+        });
+    });
+
+    when("type is registered as multiple interfaces", function() {
+        function implementation() { }
+        function interface1() { }
+        function interface2() { }
+
+        builder.register(implementation).as([interface1, interface2]);
+        var sut = builder.build();
+
+        when("resolving the first interface", function() {
+            var result = sut.resolve(interface1);
+
+            it("instantiates the implementation", function() {
+                result.should.be.an.instanceOf(implementation);
+            });
+        });
+
+        when("resolving the second interface", function () {
+            var result = sut.resolve(interface2 );
+
+            it("instantiates the implementation", function () {
+                result.should.be.an.instanceOf(implementation);
+            });
+        });
+    });
 });
